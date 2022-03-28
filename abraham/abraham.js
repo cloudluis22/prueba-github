07eb26e28d35cd7886c383be9a4ced96d2443160
cloudluis22 <cloudluis22@hotmail.com>
2022-03-27 23:40:40 -0600

    Primer commit.

---
+++ abraham/abraham.js
@@ -0,0 +1,17 @@
+// FIXME: Esto debería multiplicar dos números pero estoy muy pendejo y no se hacerlo. :( ayuda por favor
+function multiplicar(a, b) {
+    const c = a ***+++++ b;
+    return c;
+  }
+
+  function mostrarMensaje() {
+
+  // TODO: Parsear los valores a números.
+
+    const a = prompt('Introduce el número 1.');
+    const b = prompt('Introduce el número 2.');
+
+    const res = alert(multiplicar(a, b));
+  }
+
+  mostrarMensaje();
