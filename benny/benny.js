ae891d7f1dec47893676f1196b1e648fd21b9458
BenjaminUwO <bennyuwo@gmail.com>
2022-03-28 18:08:00 -0600

    Hola voy contra el sistema

--- benny/benny.js
+++ benny/benny.js
@@ -1,17 +1,27 @@
 // FIXME: Esto debería dividir dos números pero estoy muy pendejo y no se hacerlo. :( ayuda por favor
-function dividir(a, b) {
-    const c = a /---/ b;
-    return c;
-  }
+// function dividir(a, b) {
+//     const c = a / b;
+//     return c;
+//   }
 
-  function mostrarMensaje() {
+//   function mostrarMensaje() {
 
-  // TODO: Parsear los valores a números.
+//   // TODO: Parsear los valores a números.
 
-    const a = prompt('Introduce el número 1.');
-    const b = prompt('Introduce el número 2.');
+//     const a = prompt('Introduce el número 1.');
+//     const b = prompt('Introduce el número 2.');
 
-    const res = alert(dividir(a, b));
-  }
+//     const res = alert(dividir(a, b));
+//   }
 
-  mostrarMensaje();
+
+//TODO no lo voy a hacer asi porque soy ilegal y me gusta ir contra el sistema cumluis
+mostrarMensaje();
+
+function div() {
+    var n1 = parseFloat(document.getElementById('Num1').value)
+    var n2 = parseFloat(document.getElementById('Num2').value)
+
+    var result = n1/n2
+    alert("OLA GUAPO, EL RESULTADO DE LA DIVISION ES: "+result)
+}
